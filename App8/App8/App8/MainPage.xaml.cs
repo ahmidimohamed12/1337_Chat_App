1769494f3828b667f0740be4c103d137b67c7878
Ahmidi Mohamed <ahmidinador123@gmail.com>
2019-11-18 08:24:16 +0100

    /.

--- App8/App8/App8/MainPage.xaml.cs
+++ App8/App8/App8/MainPage.xaml.cs
@@ -1,6 +1,18 @@
 ﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Firebase.Database.Http;
+using Firebase.Database.Streaming;
+using Firebase.Database.Offline;
 using Xamarin.Forms;
-
+using System;
+using Newtonsoft.Json;
+using App8.Model;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json.Converters;
+using System.Linq;
 namespace App8
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -8,9 +20,37 @@ namespace App8
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+       public FirebaseClient firebase = new FirebaseClient("https://project-655056407821009696.firebaseio.com/");
         public MainPage()
         {
             InitializeComponent();
+
+        }
+        //public async List<Etudiant> etudiant()
+        //{
+
+        //    return (await firebase
+        //      .Child("Etudiant")
+        //      .OnceAsync<Etudiant>()).Select(item => new Etudiant
+        //      {
+        //          login = item.Object.login,
+        //          password = item.Object.password
+        //      }).ToArray();
+        //}
+        public async Task AddPerson(string login, string password)
+        {
+
+            await firebase
+              .Child("Etudiant")
+              .PostAsync(new Etudiant() { login = login, password = password });
+        }
+
+        private async void btn_click(object sender,EventArgs e)
+        {
+
+           await  AddPerson(tx1.Text,tx2.Text);
+
         }
     }
 }
+
---
+++ App8/App8/App8/Model/Etudiant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App8.Model
+{
+    class Etudiant
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+
+    }
+}
